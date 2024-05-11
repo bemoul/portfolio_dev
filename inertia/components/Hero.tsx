0f023ee0a4a9fdac2fd59ce9e14d5f9c0b2bd8e3
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-11 11:50:49 +0200

    Add Hero component

---
+++ inertia/components/Hero.tsx
@@ -0,0 +1,21 @@
+import { Code } from "./Code"
+import { Section } from "./Section"
+
+export const Hero = () => {
+  return (
+    <Section className="flex max-lg:flex-col items-start gap-4">
+      <div className="flex-[3] w-full flex flex-col gap-2">
+        <h2 className="font-caption text-5xl text-primary">Selim Ramdani</h2>
+        <h3 className="text-3xl items-start gap-4">Développeur Web</h3>
+        <p>Salut tout le monde ! Moi, c'est <Code>Selim</Code>, le Django Reinhardt du <Code>développement web</Code>, âgé de 26 ans, avec un sens de l'humour aussi affûté qu'un code bien écrit. Prêt à rendre vos projets aussi plaisants que cette blague !</p>
+      </div>
+      <div className="flex-[2] max-md:m-auto ml-auto">
+        <img
+          src="https://i.ibb.co/WfgzyT6/hero.jpg"
+          className="w-full h-auto max-w-xs border border-accent rounded-full shadow-lg max-md:w-40 object-cover"
+          alt="selim's picture"
+        />
+      </div>
+    </Section>
+  )
+}

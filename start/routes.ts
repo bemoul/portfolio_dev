bee8fde7d9e6598f11ea75831f718cfea380a4f3
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-19 07:27:05 +0200

    add routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,21 @@
 |
 */
 
+import LoginController from '#controllers/auth/login_controller'
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home', { version: 6 })
+import { middleware } from './kernel.js'
+import BlogController from '#controllers/blog_controller'
+import DashboardController from '#controllers/admin/dashboard_controller'
 
+router.on('/').renderInertia('home', { version: 6 })
+// router.get('/').renderInertia('home', { version: 6 })
+
+router.group(() => {
+  router.get('admin', [LoginController, 'showAdmin'] ).as('auth.admin')
+  router.post('admin', [LoginController, 'handleForm'])
+})
+.middleware(middleware.guest())
+
+router.get('blog', [BlogController, 'showBlog']).as('blog.show')
+
+router.get('dashboard', [DashboardController, 'showDashboard']).as('dashboard.show')

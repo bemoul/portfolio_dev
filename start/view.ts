4efce9cdf9ccada21b932759a05867599ad05fb7
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-19 07:26:42 +0200

    add route function helper

--- start/view.ts
+++ start/view.ts
@@ -3,9 +3,3 @@ import router from "@adonisjs/core/services/router"
 export function route(...args: Parameters<typeof router.makeUrl>) {
   return router.makeUrl(...args)
 }
-
-// export function csrfField() {
-//   const { request } = HttpContext.getOrFail()
-
-//   return Html.createElement('input', { type: 'hidden', value: request.csrfToken, name: '_csrf' })
-// }

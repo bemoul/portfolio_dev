b3b2849462ff927b0cc3980c49d706a6ce519bf0
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-21 21:26:08 +0200

    Add necessaries routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,22 +7,53 @@
 |
 */
 
-import LoginController from '#controllers/auth/login_controller'
-import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
-import BlogController from '#controllers/blog_controller'
-import DashboardController from '#controllers/admin/dashboard_controller'
+import router from '@adonisjs/core/services/router';
+import { middleware } from './kernel.js';
 
-router.on('/').renderInertia('home', { version: 6 })
-// router.get('/').renderInertia('home', { version: 6 })
+import LoginController from '#controllers/auth/login_controller';
+import LogoutController from '#controllers/auth/logout_controller';
+import BlogController from '#controllers/blog_controller';
+import DashboardController from '#controllers/admin/dashboard_controller';
+import ArticlesController from '#controllers/articles_controller';
+import WorksController from '#controllers/works_controller';
+import ProjectsController from '#controllers/projects_controller';
 
+/**
+ * Home page route
+ */
+router.on('/').renderInertia('home', { version: 6 });
+
+/**
+ * Authentication routes for administrator access
+ */
 router.group(() => {
-  router.get('admin', [LoginController, 'showAdmin'] ).as('auth.admin')
-  router.post('admin', [LoginController, 'handleForm'])
+  router.get('admin', [LoginController, 'showAdminAccess']).as('auth.admin');
+  router.post('admin', [LoginController, 'handleForm']);
 })
-.middleware(middleware.guest())
+.middleware(middleware.guest());
 
-router.get('blog', [BlogController, 'showBlog']).as('blog.show')
+/**
+ * Logout route
+ */
+router.post('logout', [LogoutController, 'logout']).middleware(middleware.auth());
 
-router.get('dashboard', [DashboardController, 'showDashboard']).as('dashboard.show')
-.middleware(middleware.auth())
+/**
+ * Blog routes
+ */
+router.get('blog', [BlogController, 'index']);
+router.get('blog/articles/:id', [BlogController, 'show']);
+
+/**
+ * Dashboard routes (requires authentication)
+ */
+router.group(() => {
+  router.get('dashboard', [DashboardController, 'showDashboard']);
+
+  router.get('/dashboard/articles', [ArticlesController, 'index']);
+  router.get('/dashboard/articles/:id', [ArticlesController, 'show']);
+
+  router.get('/dashboard/projects', [ProjectsController, 'index']);
+
+  router.get('/dashboard/works', [WorksController, 'index']).as('works.show');
+})
+.middleware(middleware.auth());

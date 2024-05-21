6c5e206285c902cc1a86b2963c6f212a42cac6d7
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-21 21:13:19 +0200

    create articles table

---
+++ database/migrations/1716148993985_create_articles_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'articles'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+      table.string('title')
+      table.text('content').nullable()
+      table.string('url')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

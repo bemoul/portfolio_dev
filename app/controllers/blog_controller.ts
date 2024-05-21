2be909c1a42c5d064584984ac9eb82916015ab84
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-21 21:16:16 +0200

    add blog controller

--- app/controllers/blog_controller.ts
+++ app/controllers/blog_controller.ts
@@ -1,9 +1,27 @@
+import Article from "#models/article";
+import { ArticlePresenter } from "#presenters/article_presenter";
+import { ArticlesPresenter } from "#presenters/articles_presenter";
 import { inject } from "@adonisjs/core";
 import { HttpContext } from "@adonisjs/core/http";
 
-@inject()
 export default class BlogController {
-  async showBlog({inertia }: HttpContext){
-    return inertia.render('blog')
+  @inject()
+  async index({ inertia, request }: HttpContext, presenter: ArticlesPresenter) {
+    const page = request.input('page', 1)
+
+    const articles = await Article.query().preload('user').paginate(page, 50)
+
+    return inertia.render('blog/index', {
+      articles: presenter.toJson(articles)
+    });
+  }
+
+  @inject()
+  async show({ inertia, params }: HttpContext, presenter: ArticlePresenter) {
+    const article = await Article.query().preload('user').where('id', params.id).firstOrFail()
+
+    return inertia.render('blog/show', {
+      article: presenter.toJson(article)
+    });
   }
 }

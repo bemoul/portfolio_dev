b16e0d7c93c83bd7cc0154f2e04b0168331b5791
Selim-Ramdani <selimcodeur@gmail.com>
2024-06-28 17:45:42 +0200

    add meta data with Head component from inertiajs

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -9,7 +9,12 @@ import { HowCanIHelp } from '~/components/HowCanIHelp'
 export default function Home() {
   return (
     <main>
-      <Head title="Selim's portfolio" />
+      <Head>
+        <title>Selim Ramdani : Portfolio</title>
+        <meta head-key="description" name="description" content="Développeur Web spécialisé dans la création d'applications Web. Passionné et disponible pour travailler sur vos projets. " />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="https://i.ibb.co/X2GtTjk/sm.png"/>
+    </Head>
 
       <Header />
 
